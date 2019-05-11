67b717a82b80c5bf98406a2c36aae72fe7544637
KamilJed <kamijedrzejczak98@wp.pl>
2019-05-11 17:14:19 +0200

    Change Vec2d to Vector2

--- core/src/com/thebiggestsnake/snake/Snake.java
+++ core/src/com/thebiggestsnake/snake/Snake.java
@@ -1,13 +1,13 @@
-package com.thebiggestsnake;
+package com.thebiggestsnake.snake;
 
 import com.badlogic.gdx.math.Circle;
+import com.thebiggestsnake.theBiggestSnake;
 
 import java.util.ArrayList;
 
 public class Snake {
     private ArrayList<Circle> circles;
     private theBiggestSnake game;
-
 
     public Snake(theBiggestSnake g){
         this.game = g;
