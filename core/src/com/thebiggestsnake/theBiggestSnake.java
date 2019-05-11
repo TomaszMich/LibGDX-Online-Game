c9d22fa138ea948e1d4e67868ca577083f6c5797
Tomasz Michalski <tomasz.michals@gmail.com>
2019-05-11 19:25:16 +0200

    Adding views and created simple menu.

--- core/src/com/thebiggestsnake/theBiggestSnake.java
+++ core/src/com/thebiggestsnake/theBiggestSnake.java
@@ -1,32 +1,65 @@
 package com.thebiggestsnake;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.thebiggestsnake.views.*;
 
-public class theBiggestSnake extends ApplicationAdapter {
+
+public class theBiggestSnake extends Game {
 	private SpriteBatch batch;
-	private Texture img;
+    private LoadingScreen loadingScreen;
+    private PreferencesScreen preferencesScreen;
+    private MenuScreen menuScreen;
+    private MainScreen mainScreen;
+    private EndScreen endScreen;
+
+    public final static int MENU = 0;
+    public final static int PREFERENCES = 1;
+    public final static int APPLICATION = 2;
+    public final static int ENDGAME = 3;
 	
 	@Override
 	public void create () {
+        loadingScreen = new LoadingScreen(this);
+        setScreen(loadingScreen);
 		batch = new SpriteBatch();
 	}
 
-	@Override
+/*	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
 		batch.end();
-	}
+	}*/
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
+
+    public void changeScreen(int screen){
+        switch(screen){
+            case MENU:
+                if(menuScreen == null) menuScreen = new MenuScreen(this);
+                this.setScreen(menuScreen);
+                break;
+            case PREFERENCES:
+                if(preferencesScreen == null) preferencesScreen = new PreferencesScreen(this);
+                this.setScreen(preferencesScreen);
+                break;
+            case APPLICATION:
+                if(mainScreen == null) mainScreen = new MainScreen(this);
+                this.setScreen(mainScreen);
+                break;
+            case ENDGAME:
+                if(endScreen == null) endScreen = new EndScreen(this);
+                this.setScreen(endScreen);
+                break;
+        }
+    }
 }
---
+++ core/src/com/thebiggestsnake/views/EndScreen.java
@@ -0,0 +1,47 @@
+package com.thebiggestsnake.views;
+
+import com.badlogic.gdx.Screen;
+import com.thebiggestsnake.theBiggestSnake;
+
+public class EndScreen implements Screen {
+    private theBiggestSnake parent;
+
+    public EndScreen(theBiggestSnake parent) {
+        this.parent = parent;
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
