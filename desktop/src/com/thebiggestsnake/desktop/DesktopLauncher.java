a27f08d45e0c31379ffcf4ad1306d4d191eed870
Tomasz Michalski <tomasz.michals@gmail.com>
2019-05-11 15:47:21 +0200

    Changing size of the window

--- desktop/src/com/thebiggestsnake/desktop/DesktopLauncher.java
+++ desktop/src/com/thebiggestsnake/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.thebiggestsnake.theBiggestSnake;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.title = "The Biggest Snake";
+        config.width = 1920;
+        config.height = 1080;
 		new LwjglApplication(new theBiggestSnake(), config);
 	}
 }
